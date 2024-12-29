587a3731d303f211b80ab2a2d4e7b8f27e7a339c
Michal Protasowicki <71427889+michpro@users.noreply.github.com>
2024-12-29 11:04:16 +0100

    improved menu behaviour - deleting selected rows

--- gui/panel_right.py
+++ gui/panel_right.py
@@ -3,8 +3,10 @@
 
 import csv
 import customtkinter as ctk
+from CTkMessagebox import CTkMessagebox
 from .treeview import CTkTreeview
 from .popup import CTkFloatingWindow
+
 
 class Messages(ctk.CTkFrame):
     def __init__(self, parent):
@@ -19,23 +21,16 @@ class Messages(ctk.CTkFrame):
                   ('type', 'VSCP Type', 290, 290, 'center', 'w'),
                   ('data', 'Data', 280, 280, 'center', 'w')
                  ]
-        # self.data = [
-        #     { 'values': ['18:47:49.813176', 'Rx', '0xFF', 'Even higher', 'CLASS1.WEATHER_FORECAST', 'ASTRONOMICAL_SUNRISE_TWILIGHT_START', '0xFF 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF']}
-        # ]
         self.widget = ctk.CTkFrame(parent, fg_color='transparent')
         self.widget.pack(padx=(0, 4), pady=4, fill='both', expand=True)
         self.messages = CTkTreeview(self.widget, header, xscroll=False)
         self.messages.pack(padx=2, pady=2, fill='both', expand=True)
         self.messages.treeview.bind('<Double-Button-1>', self.item_deselect)
-        # self.messages.treeview.bind('<<TreeviewSelect>>', self.tselect)
-
-        # for _ in range(30):
-        #     self.insert(self.data)
-
         self.messages.treeview.bind('<Button-3>', lambda event: self._show_menu(event, self.dropdown))
+
         self.dropdown = CTkFloatingWindow(self.widget)
         self.dropdown_bt_clear_all = ctk.CTkButton(self.dropdown.frame, border_spacing=0, corner_radius=0,
-                                              text="Clear all items", command=self._clear_all_items)
+                                                   text="Clear all items", command=self._clear_all_items)
         self.dropdown_bt_clear_all.pack(expand=True, fill="x", padx=0, pady=0)
         self.dropdown_bt_clear_selected = ctk.CTkButton(self.dropdown.frame, border_spacing=0, corner_radius=0,
                                                         text="Clear selected items", command=self._clear_selected_items)
@@ -66,6 +61,7 @@ class Messages(ctk.CTkFrame):
         for row in selected_rows:
             self.messages.treeview.delete(row)
 
+
     def _save_log(self):
         filetypes = [('CSV Log File', '*.csv')]
         fname = ctk.filedialog.asksaveasfilename(confirmoverwrite=True, initialfile='vscp_log',
@@ -76,13 +72,17 @@ class Messages(ctk.CTkFrame):
                 for row_id in self.messages.treeview.get_children():
                     row = self.messages.treeview.item(row_id)['values']
                     csvwriter.writerow(row)
+        else:
+            CTkMessagebox(title='Error', message='No Log file name !!!', icon='cancel')
+
 
     def _show_menu(self, event, menu):
         try:
             state = 'normal' if 0 != len(self.messages.treeview.get_children()) else 'disabled'
             self.dropdown_bt_clear_all.configure(state=state)
-            self.dropdown_bt_clear_selected.configure(state=state)
             self.dropdown_bt_save_log.configure(state=state)
+            state = 'normal' if 0 != len(self.messages.treeview.selection()) else 'disabled'
+            self.dropdown_bt_clear_selected.configure(state=state)
             menu.popup(event.x_root, event.y_root)
         finally:
             menu.grab_release()
@@ -97,5 +97,5 @@ class RightPanel(ctk.CTkFrame):
 
         self.messages = Messages(self.widget)
 
-        self.info = ctk.CTkFrame(self.widget, height=150)#, corner_radius=5)#, fg_color='SkyBlue')
+        self.info = ctk.CTkFrame(self.widget, height=150)
         self.info.pack(padx=(2, 6), pady=(1, 5), side='top', anchor='s', fill='both', expand=False)
